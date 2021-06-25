c3919dcfa530e1595ff6dfecb09eedd2037f743a
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-06-24 23:56:02 -0400

    added nullcheck

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     useEffect(() => {
         setSocket(socketClient(SERVER));
         return () => {
-            socket.disconnect();
+            socket && socket.disconnect();
             setSocket(null);
         };
     }, [socket]);
