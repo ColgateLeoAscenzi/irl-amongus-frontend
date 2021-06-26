704a0252cc9e4e93754f32da5c776defd0961d77
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-06-26 16:48:13 -0400

    Adjusted api URL, added status button

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './styles';
 import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
@@ -8,15 +8,33 @@ import SocketContext from '../../contexts/SocketContext';
 const Admin = ({ classes }) => {
     const { socket } = useContext(SocketContext);
 
+    const [status, setStatus] = useState('Checking Server Status...');
+
     const clickHandler = () => {
         socket && socket.emit('reset-rooms');
     };
 
+    const statusHandler = async () => {
+        try {
+            const resp = await fetch(process.env.REACT_APP_API_URL);
+            const msg = await resp.json();
+            setStatus(`Server is ${msg.status}`);
+        } catch (e) {
+            setStatus('Server is Down');
+        }
+    };
+
+    useEffect(() => {
+        statusHandler().then(() => {});
+    });
+
     return (
         <div className={classes.root}>
+            <div>Status: {status}</div>
             <Button className={classes.dangerButton} onClick={clickHandler}>
                 Reset Rooms
             </Button>
+            <Button onClick={statusHandler}>Check Status</Button>
         </div>
     );
 };
